737b22c02de1aa2ec0577f9eb7e0a5dbc74f743e
Elliot <66279196+Elliot-Hyphen@users.noreply.github.com>
2021-05-23 19:47:04 +0100

    Update kalifix.sh

--- kalifix.sh
+++ kalifix.sh
@@ -15,8 +15,8 @@ do
         "Fix Wifi Problems")
             echo " Please DONT Panic if your system Reboots! ..."
             sudo apt-get update
-            sudo apt-get upgrade && sudo apt-get dist-upgrade
-            apt-get install linux-image-$(uname -r|sed 's,[^-]*-[^-]*-,,') linux-headers-$(uname -r|sed 's,[^-]*-[^-]*-,,') broadcom-sta-dkms
+            sudo apt-get upgrade -y ; sudo apt-get dist-upgrade -y
+            apt-get install linux-image-$(uname -r|sed 's,[^-]*-[^-]*-,,') linux-headers-$(uname -r|sed 's,[^-]*-[^-]*-,,') broadcom-sta-dkms -y
             sudo modprobe -r b44 b43 b43legacy ssb brcmsmac bcma
             sudo modprobe wl
 echo "Rebooting..... <:)" 
@@ -25,7 +25,7 @@ reboot
 ;;
   "Update OS")
 echo "Updating...."
-        sudo apt-get update && sudo apt-get upgrade && sudo apt-get dist-upgrade
+        sudo apt-get update ; sudo apt-get upgrade -y ; sudo apt-get dist-upgrade -y 
 clear
 echo "!!!SYSTEM UPDATED!!!"
 lsb_release -a   
@@ -36,15 +36,15 @@ echo "!!!SYSTEM UPDATED!!!"
 echo "Installing Kali-Linux Tools"
 	break
 	sudo apt-get update && sudo apt-get upgrade -y 
-	sudo apt-get install kali-linux-all
+	sudo apt-get install kali-linux-all -y
 echo "Rebooting..."	
 	sudo reboot
             
 ;;
   "Clean System") 
-	sudo apt-get autoclean && sudo apt-get autoremove
+	sudo apt-get autoclean -y ; sudo apt-get autoremove -y
 echo " Installing Bleachbit.... Please Wait!"
-	sudo apt-get install bleachbit
+	sudo apt-get install bleachbit -y
 	sudo bleachbit
 
 ;;
