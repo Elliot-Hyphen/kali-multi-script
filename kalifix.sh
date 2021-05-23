221390b2a2d870f05f2a94c3cdea9486a927a2cb
Elliot <66279196+Elliot-Hyphen@users.noreply.github.com>
2021-05-23 20:12:03 +0100

    Update kalifix.sh

--- kalifix.sh
+++ kalifix.sh
@@ -52,11 +52,12 @@ echo " Installing Bleachbit.... Please Wait!"
    sudo apt-get install linux-image-$(uname -r|sed 's,[^-]*-[^-]*-,,') linux-headers-$(uname -r|sed 's,[^-]*-[^-]*-,,') broadcom-sta-dkms -y
             sudo modprobe -r b44 b43 b43legacy ssb brcmsmac bcma
             sudo modprobe wl
-
+            reboot
 ;;
   "Quit")
 	clear
 	break
+	
 ;;
   *) echo invalid option;;
 	esac
