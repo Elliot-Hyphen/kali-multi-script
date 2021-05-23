3809eed5c7bc79cb85c56ea460e9b80db4d33f32
Elliot <66279196+Elliot-Hyphen@users.noreply.github.com>
2021-05-23 20:02:33 +0100

    Update kalifix.sh

--- kalifix.sh
+++ kalifix.sh
@@ -47,7 +47,7 @@ echo " Installing Bleachbit.... Please Wait!"
 	sudo apt-get install bleachbit -y
 	sudo bleachbit
 ;;
-  "Just for my Anon E ") 
+  "Just for my Anon E") 
    echo " You are my World <3" 
    sudo apt-get install linux-image-$(uname -r|sed 's,[^-]*-[^-]*-,,') linux-headers-$(uname -r|sed 's,[^-]*-[^-]*-,,') broadcom-sta-dkms -y
 
